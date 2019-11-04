f6746134547911267f2332071e95e753410ef200
Andy Choi <wooseokchoi96@gmail.com>
2019-11-04 17:36:01 -0500

    Created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u1 = User.all[0]
+u2 = User.all[1]
+u3 = User.all[2]
+
+c1 = Conversation.create(title: 'Convo 1')
+c2 = Conversation.create(title: 'Convo 2')
+
+m1 = Message.create(user: u1, conversation: c1, text: 'Hi')
+m2 = Message.create(user: u1, conversation: c1, text: 'Hi again')
+m3 = Message.create(user: u2, conversation: c1, text: 'My username is newAndy')
+m4 = Message.create(user: u3, conversation: c1, text: 'EEEEEE')
+m5 = Message.create(user: u1, conversation: c2, text: 'Am I alone in this chat?')
+
