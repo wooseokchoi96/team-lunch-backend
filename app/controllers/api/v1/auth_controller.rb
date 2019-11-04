7cf98938c9731c16f8311b17b7bb264b59e943d3
Andy Choi <wooseokchoi96@gmail.com>
2019-11-03 21:09:11 -0500

    Fixed encode_token(user_id) to encode_token(user.id)

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::AuthController < ApplicationController
             password: params[:password]
         )
         if user.save
-            token = encode_token(user_id)
+            token = encode_token(user.id)
             render json: {user: user, token: token}
         else 
             render json: {errors: user.errors.full_messages}
@@ -17,7 +17,7 @@ class Api::V1::AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
-            token = encode_token(user_id)
+            token = encode_token(user.id)
             render json: {user: user, token: token}
         else
             render json: {errors: 'Login Failed'}
